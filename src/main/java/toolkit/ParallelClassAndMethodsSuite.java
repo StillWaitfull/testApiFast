071d13f6bfe47e8c5287c63f5d213b17eed825eb
Sergey Kashapov <s.kashapov@corp.mail.ru>
2018-06-20 08:41:32 +0300

    fix

--- src/main/java/toolkit/ParallelClassAndMethodsSuite.java
+++ src/main/java/toolkit/ParallelClassAndMethodsSuite.java
@@ -1,10 +1,15 @@
 package toolkit;
 
+import org.junit.runner.Runner;
+import org.junit.runners.ParentRunner;
 import org.junit.runners.Suite;
 import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.RunnerBuilder;
 import org.junit.runners.model.RunnerScheduler;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -33,6 +38,26 @@ public class ParallelClassAndMethodsSuite extends Suite {
     public ParallelClassAndMethodsSuite(Class<?> klass, RunnerBuilder builder) throws InitializationError {
         super(klass, builder);
         setScheduler(schedulerForClasses);
+        List<Runner> children = getChildren();
+        children.forEach(this::parallelize);
+    }
 
+    private  Runner parallelize(Runner runner) {
+        if (runner instanceof ParentRunner) {
+            ((ParentRunner<?>) runner).setScheduler(new RunnerScheduler() {
+                private final List<CompletableFuture> completableFutures = new ArrayList<>();
+
+                @Override
+                public void schedule(Runnable childStatement) {
+                    completableFutures.add(CompletableFuture.runAsync(childStatement));
+                }
+
+                @Override
+                public void finished() {
+                    CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[0])).join();
+                }
+            });
+        }
+        return runner;
     }
 }
